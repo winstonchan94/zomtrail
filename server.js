1861b0ab46ba6d21e8811ced4c0f9fda565be9ca
Walter Wan <WalterHWan@gmail.com>
2018-05-18 17:35:08 -0700

    Fix port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log('Express server listening on port ' + port);
