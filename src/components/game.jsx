fe60570d5b3896650cc4b8c4eb2a5df0a5fa2865
Winston Chan <winston.chan94@gmail.com>
2018-05-24 12:04:11 -0700

    dingla

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import Gmap from './gmap';
+const axios = require('axios');
 
 class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      path: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('hello');
+    axios({
+      method: 'POST',
+      url: '/api/paths',
+      data: this.state
+    }).then(res => console.log(res));
+  }
+
   render() {
     return (
       <div className='story-div'>
@@ -22,6 +46,8 @@ class Game extends Component {
           <div className="dir-textfield mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
             <textarea
               className="dir-textarea mdl-textfield__input"
+              value={this.state.directions}
+              onChange={this.update('path')}
               type="text" rows= "4" id="sample1" />
             <label
               className="dir-label mdl-textfield__label"
@@ -30,8 +56,8 @@ class Game extends Component {
           </div>
         </form>
         <div className='summit-action-buttons'>
-          <button className="summit-button mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-            Summit
+          <button onClick={this.handleSubmit} className="summit-button mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+            Submit
           </button>
         </div>
       </div>
