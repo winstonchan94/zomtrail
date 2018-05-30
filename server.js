e4f2f982648fe41ff22530997fbe63bea2cf36f5
Winston Chan <winston.chan94@gmail.com>
2018-05-30 01:23:51 -0700

    Remove crap

--- server.js
+++ server.js
@@ -16,9 +16,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // CLOUDINARY
-console.log("env = " + process.env.CLOUDINARY_URL);
-const CLOUDINARY_URL = process.env.CLOUDINARY_URL ||
- require('../../config/cloudinary.js');
 
 // MongoDB
 const db = process.env.MONGODB_URI || require('./config/db');
